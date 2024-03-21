38faec4b3c3eaf3f1db41f3c1fd029c6536801bc
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-21 17:55:23 +0900

    Update : User class constructor

--- shop/src/shop/User.java
+++ shop/src/shop/User.java
@@ -2,9 +2,14 @@ package shop;
 
 public class User {
 	private String id, password;
+	private Cart cart;
 	
 	public final String ADMIN = "admin";
 	public final String MENUS = "24567";
+	
+	public User() {
+		
+	}
 	
 	public User(String id, String password) {
 		this.id = id;
@@ -17,6 +22,18 @@ public class User {
 
 	public String getPassword() {
 		return password;
+	}
+	
+	public Cart getCart() {
+		return cart;
+	}
+	
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+	
+	public User clone() {
+		return new User(this.id, this.password);
 	}
 	
 	@Override
