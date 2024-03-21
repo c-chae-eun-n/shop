a723c54b095648b57298f2eb0e170e4e791c4109
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-21 16:40:41 +0900

    Modify : User Class

--- shop/src/shop/User.java
+++ shop/src/shop/User.java
@@ -1,5 +1,26 @@
 package shop;
 
 public class User {
+	private String id, password;
+	
+	public final String ADMIN = "admin";
+	public final String MENUS = "24567";
+	
+	public User(String id, String password) {
+		this.id = id;
+		this.password = password;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s/%s", this.id, this.password);
+	}
 }
