d090fb8fce12f6a8effc77f785f9c68ccda5f141
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-21 18:17:05 +0900

    Update : UserManager class  Read&Update&Delete

--- shop/src/shop/Shop.java
+++ shop/src/shop/Shop.java
@@ -1,5 +1,25 @@
 package shop;
 
 public class Shop {
-
+	
+	private final int JOIN = 1;
+	private final int LEAVE = 2;
+	private final int LOG_IN = 3;
+	private final int LOG_OUT = 4;
+	private final int SHOPPING = 5;
+	private final int MY_PAGE = 6;
+	private final int FILE = 7;
+	
+	private final int MY_CART = 1;
+	private final int DELETE_ITEM = 2;
+	private final int UPDATE_ITEM = 3;
+	
+	
+	public Shop(String message) {
+		
+	}
+	
+	public void run() {
+		
+	}
 }
--- shop/src/shop/UserManager.java
+++ shop/src/shop/UserManager.java
@@ -1,5 +1,41 @@
 package shop;
 
-public class UserManager {
+import java.util.ArrayList;
 
+public class UserManager {
+	private ArrayList<User> userList;
+	
+	private UserManager() {
+		userList = new ArrayList<>();
+	}
+	
+	private static UserManager instance = new UserManager();
+	
+	public static UserManager getInstance() {
+		return instance;
+	}
+	
+	// Read
+	public User getUser(int index) {
+		User user = userList.get(index);
+		return user.clone();
+	}
+	
+	// Update
+	public void setUser(int index, User user) {
+		User temp = userList.get(index);
+		
+		if(temp.getId().equals(user.getId()))
+			temp.setCart(user.getCart());
+	}
+	
+	// Delete
+	public void removeUser(int index) {
+		if(index < 0 || index >= userList.size()) {
+			System.err.println("유효하지 않은 범위입니다.");
+			return;
+		}
+		
+		userList.remove(index);
+	}
 }
