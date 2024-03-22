33989f3783e44046bd37c7c2959d1aa2f91e4214
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-22 18:04:04 +0900

    Add : Cart class Create 메서드 추가

--- shop/src/shop/Cart.java
+++ shop/src/shop/Cart.java
@@ -4,4 +4,36 @@ import java.util.ArrayList;
 
 public class Cart {
 	private ArrayList<Item> cartList;
+	
+	public Cart() {
+		cartList = new ArrayList<>();
+	}
+	
+	// Create
+	public void createCart(String name, int piece) {
+		int index = findItemIndexByName(name);
+		System.out.println(index);
+		if(index == -1) {
+			Item item = new Item();
+			item.setName(name);
+			item.setPiece(piece);
+			
+			System.out.println("size : " + cartList.size());
+			cartList.add(item);
+			System.out.println("size : " + cartList.size());
+			return;
+		}
+		int curPiece = cartList.get(index).getPiece();
+		cartList.get(index).setPiece(curPiece+piece);
+		return;
+	}
+	
+	private int findItemIndexByName(String name) {
+		for(int i=0; i<cartList.size(); i++) {
+			Item item = cartList.get(i);
+			if(item.getName().equals(name))
+				return i;
+		}
+		return -1;
+	} 
 }
