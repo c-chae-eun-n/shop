adb7dd77251596bf27ce58a4777e189eda0e0b82
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-22 11:58:45 +0900

    Update : ItemManager class Crate & findByName 메서드 추가

--- shop/src/shop/ItemManager.java
+++ shop/src/shop/ItemManager.java
@@ -14,4 +14,38 @@ public class ItemManager {
 	public static ItemManager getInstance() {
 		return instance;
 	}
+	
+	// create
+	public Item createItem(String name, int price) {
+		if(isValidName(name)) {
+			Item item = new Item(name, price);
+			itemList.add(item);
+			return item.clone();
+		}
+		return new Item();
+	}
+	
+	private boolean isValidName(String name) {
+		Item item = findItemByName(name);
+		if(item.getName() == null)
+			return true;
+		return false;
+	}
+	
+	public Item findItemByName(String name) {
+		for(Item item : itemList) {
+			if(item.getName().equals(name))
+				return item.clone();
+		}
+		return new Item();
+	}
+	
+	public int findItemIndexByName(String name) {
+		for(int i=0; i<itemList.size(); i++) {
+			Item item = itemList.get(i);
+			if(item.getName().equals(name))
+				return i;
+		}
+		return -1;
+	}
 }
